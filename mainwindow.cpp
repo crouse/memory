72116ef2e5ca4cbf776174610b24b397483b577e
quqinglei <quqinglei@icloud.com>
2016-03-10 22:05:25 +0800

    update
    记录已经做好。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QStandardPaths>
 #include <QMessageBox>
 #include <QSqlQuery>
+#include <QSqlRecord>
 #include <QDebug>
 #include <QColor>
 #include <QLabel>
@@ -43,6 +44,9 @@ MainWindow::MainWindow(QWidget *parent) :
 
     ui->actionList->setText(QChar(fa::barchart));
     ui->actionList->setFont(awesome->font(16));
+
+    ui->dateEditEcurrent->setDate(QDate::currentDate());
+    ui->dateEditEBirth->setDate(QDate(1900, 1, 1));
 }
 
 MainWindow::~MainWindow()
@@ -93,31 +97,51 @@ void MainWindow::setDatabase()
         signTableFieldsStringList << "姓名" << "性别" << "手机" << "生日" << "日期" << "当前记录时间";
         signDictTableFieldsStringList << "姓名" << "性别" << "手机" << "生日" << "签到次数";
 
-        setModel(ui->tableViewSigns, modelEdit, "sign", &signTableFieldsStringList, "");
-        setModel(ui->tableViewDict, modelChoose, "sign_dict", &signDictTableFieldsStringList, "");
+        setModel(ui->tableViewSigns, "modelEdit", "sign", &signTableFieldsStringList, "", 0);
+        setModel(ui->tableViewDict, "modelChoose", "sign_dict", &signDictTableFieldsStringList, "", 4);
     }
 }
 
-void MainWindow::setModel(QTableView *tableViewName, QSqlTableModel *modelName, QString tableName, QStringList *tableFields, QString filter)
+void MainWindow::setModel(QTableView *tableViewName, QString modelName, QString tableName, QStringList *tableFields, QString filter, int orderNum)
 {
-    modelName = new QSqlTableModel(this);
-    modelName->setTable(tableName);
-    modelName->setEditStrategy(QSqlTableModel::OnFieldChange);
-    modelName->setSort(0, Qt::AscendingOrder);
+    QSqlTableModel *model;
+    if (modelName == "modelEdit") {
+        modelEdit = new QSqlTableModel;
+        model = modelEdit;
+    } else if (modelName == "modelChoose") {
+        modelChoose = new QSqlTableModel;
+        model = modelChoose;
+    } else {
+        return;
+    }
+
+    model= new QSqlTableModel(this);
+    model->setTable(tableName);
+    model->setEditStrategy(QSqlTableModel::OnFieldChange);
+
+    model->setSort(orderNum, Qt::DescendingOrder);
 
     int tableFieldsNum = tableFields->length();
     for (int i = 0; i < tableFieldsNum; i++) {
-        modelName->setHeaderData(i, Qt::Horizontal, tableFields->at(i));
+        model->setHeaderData(i, Qt::Horizontal, tableFields->at(i));
     }
 
-    tableViewName->setModel(modelName);
+    tableViewName->setModel(model);
     tableViewName->alternatingRowColors();
     tableViewName->horizontalHeader()->setStretchLastSection(true);
 
-    modelName->setFilter(filter);
-    modelName->select();
+    model->setFilter(filter);
+    model->select();
     tableViewName->reset();
-    qDebug() << modelName;
+    qDebug() << modelName << model << model->rowCount();
+
+    if (modelName == "modelEdit") {
+        modelEdit = model;
+    } else if (modelName == "modelChoose") {
+        modelChoose = model;
+    } else {
+        return;
+    }
 }
 
 void MainWindow::setMainToolBar()
@@ -147,22 +171,148 @@ void MainWindow::on_actionDict_triggered()
 void MainWindow::on_actionList_triggered()
 {
     if (ui->tableViewDict->isHidden()) {
-        initDict();
+        modelChoose->setFilter("");
+        modelChoose->select();
+        ui->tableViewDict->reset();
         ui->tableViewSigns->hide();
         ui->tableViewDict->show();
+        ui->pushButtonEsave->setDisabled(true);
     } else {
         ui->tableViewDict->hide();
         ui->tableViewSigns->show();
+        ui->pushButtonEsave->setEnabled(true);
     }
+}
+
+int MainWindow::updateDict(QString name, QString gender, QString phone, QString birthday)
+{
+    int num = 0;
+    QSqlQuery query;
+    query.exec(QString("select name from sign_dict where name = '%1' and gender = '%2' and phone = '%3'").arg(name).arg(gender).arg(phone));
+    qDebug() << query.lastQuery();
+    while(query.next())  { num+=1; }
+
+    // 如果字典表里有记录，计数器加1
+    if (num > 0) {
+        query.exec(QString("update sign_dict set cnt = cnt + 1 where name = '%1' and gender = '%2' and phone = '%3'").arg(name).arg(gender).arg(phone));
+        qDebug() << query.lastQuery();
+        return num;
+    }
+
+
+    // 如果字典表里没有数据，添加到字典表
+    query.exec(QString("insert into sign_dict (name, gender, phone, birthday, cnt) values ('%1', '%2', '%3', '%4', '%5')").arg(name).arg(gender).arg(phone).arg(birthday).arg(1));
+    qDebug() << query.lastQuery();
+
+    return 0;
+}
+
+bool MainWindow::ifSignedToday(QString name, QString gender, QString phone, QString logdate)
+{
+    int cnt = 0;
+    QSqlQuery query;
+    query.exec(QString("select * from sign where name = '%1' and gender = '%2' and phone = '%3' and logdate = '%4'").arg(name).arg(gender).arg(phone).arg(logdate));
+    qDebug() << query.lastQuery();
+
+    while(query.next()) {
+        cnt+=1;
+    }
+
+    return cnt;
+}
+
+int MainWindow::choose()
+{
+    QString name = ui->lineEditEname->text();
+    modelChoose->setFilter(QString("name = '%1'").arg(name));
+    modelChoose->select();
+    int rowCount = modelChoose->rowCount();
+    ui->tableViewDict->reset();
+    qDebug() << "rowCount" << rowCount;
+    if (rowCount == 0) {
+        return 0;
+    } else if (rowCount == 1) {
+        QString gender = modelChoose->record(0).value("gender").toString();
+        QString phone = modelChoose->record(0).value("phone").toString();
+        QString birthday = modelChoose->record(0).value("birthday").toString();
+        ui->lineEditEphone->setText(phone);
+        ui->comboBoxEgender->setCurrentText(gender);
+        ui->dateEditEBirth->setDate(QDate::fromString(birthday, "yyyy-MM-dd"));
+    } else if (rowCount > 1) {
+        ui->tableViewDict->show();
+        qDebug() << "dao";
+    }
+
+    return rowCount;
 }
 
 void MainWindow::on_pushButtonEsave_clicked()
 {
-    /*
     QString name = ui->lineEditEname->text().trimmed();
     QString gender = ui->comboBoxEgender->currentText().trimmed();
     QString phone = ui->lineEditEphone->text().trimmed();
     QString birthday = ui->dateEditEBirth->date().toString("yyyy-MM-dd");
-    QString current = ui->dateEditEcurrent->dateTime().toString("yyyy-MM-dd HH:mm:ss");
-    */
+    QString logdate = ui->dateEditEcurrent->dateTime().toString("yyyy-MM-dd HH:mm:ss");
+
+    if (!ui->tableViewDict->isHidden()) {
+        ui->tableViewDict->hide();
+    }
+
+    if (name.isEmpty()) {
+        QMessageBox::information(this, "", "贤二，姓名不能为空滴啊！");
+        return;
+    }
+
+
+    bool signStat = ifSignedToday(name, gender, phone, logdate);
+
+    if (signStat) {
+        QMessageBox::information(this, "", QString("嘿嘿，%1 %2 今天签到已经录进去啦，我们是一天签到一次哦！").arg(name).arg(phone));
+        clearEdits();
+        return;
+    }
+
+    updateDict(name, gender, phone, birthday);
+
+    QString current = QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+
+    QSqlQuery query;
+    query.exec(QString("insert into sign(name, gender, phone, birthday, logdate, current) values ('%1', '%2', '%3', '%4', '%5', '%6')").arg(name).arg(gender).arg(phone).arg(birthday).arg(logdate).arg(current));
+    modelEdit->setFilter(QString("logdate = '%1'").arg(logdate));
+    modelEdit->select();
+    ui->tableViewSigns->reset();
+
+    clearEdits();
+
+}
+
+void MainWindow::clearEdits()
+{
+    ui->lineEditEname->clear();
+    ui->lineEditEphone->clear();
+    ui->dateEditEBirth->setDate(QDate(1900, 1, 1));
+    ui->lineEditEname->setFocus();
+}
+
+void MainWindow::on_tableViewDict_doubleClicked(const QModelIndex &index)
+{
+    int rowNum = index.row();
+    QString phone = modelChoose->record(rowNum).value("phone").toString();
+    QString gender = modelChoose->record(rowNum).value("gender").toString();
+    QDate birthday = modelChoose->record(rowNum).value("birthday").toDate();
+    ui->lineEditEphone->setText(phone);
+    ui->comboBoxEgender->setCurrentText(gender);
+    ui->dateEditEBirth->setDate(birthday);
+}
+
+void MainWindow::on_lineEditEname_editingFinished()
+{
+    choose();
+}
+
+void MainWindow::on_actionCurrentDateRows_triggered()
+{
+    modelEdit->setFilter(QString("logdate = '%1'").arg(ui->dateEditEcurrent->date().toString("yyyy-MM-dd 00:00:00")));
+    modelEdit->select();
+    ui->tableViewSigns->reset();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -22,9 +22,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void setDatabase();
-    void setModel(QTableView *tableViewName, QSqlTableModel *modelName, QString tableName, QStringList *tableFields, QString filter);
+    void setModel(QTableView *tableViewName, QString modelName, QString tableName, QStringList *tableFields, QString filter, int orderNum);
     void setMainToolBar();
     void initDict();
+    int updateDict(QString name, QString gender, QString phone, QString birthday);
+    bool ifSignedToday(QString name, QString gender, QString phone, QString logdate);
+    void clearEdits();
+    int choose();
     QSqlDatabase database;
     QSqlTableModel *modelEdit;
     QSqlTableModel *modelChoose;
@@ -39,6 +43,12 @@ private slots:
     void on_actionList_triggered();
 
     void on_pushButtonEsave_clicked();
+
+    void on_tableViewDict_doubleClicked(const QModelIndex &index);
+
+    void on_lineEditEname_editingFinished();
+
+    void on_actionCurrentDateRows_triggered();
 
 private:
     Ui::MainWindow *ui;
